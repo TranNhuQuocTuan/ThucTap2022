6dad1047198129545b53f23c000f6fbc477efdb9
TranNhuQuocTuan <traitrannhu@gmail.com>
2022-12-23 16:06:35 +0700

    Add project files.

---
+++ ThucTap2022/Controllers/ThucTap2022Controller1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ThucTap2022.Controllers
+{
+    public class ThucTap2022Controller1 : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
